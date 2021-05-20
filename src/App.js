66ffc4824c635fbc00e7988231c9aff5e1266b70
audrius ambra <audriusambrazevicius@gmail.com>
2021-05-20 09:43:40 +0300

    create App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import AddUser from "../src/Pages/AddUser"
+import MainPage from "../src/Pages/MainPage"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <div className="d-flex">
+                    <Link className="m-20" to="/">User List</Link>
+                    <Link className="m-20" to="/AddUser">Add User</Link>
+                </div>
+
+                <Switch>
+                    <Route exact path="/">
+                        <MainPage/>
+                    </Route>
+                    <Route path="/AddUser">
+                        <AddUser/>
+                    </Route>
+                </Switch>
+
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Pages/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            vartotojai
+        </div>
+    );
+};
+
+export default MainPage;
